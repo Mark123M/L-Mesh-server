f784e20abda10f8c10f04ade181ea7cf8e8b2652
Mark123M <mark123ymm@gmail.com>
2023-08-15 14:38:37 -0400

    adding table constraints and constant table

--- migrations/sqls/20230806035412-lsystems-up.sql
+++ migrations/sqls/20230806035412-lsystems-up.sql
@@ -8,8 +8,10 @@ CREATE TABLE IF NOT EXISTS public."LSystem"
 (
     lsystem_id SERIAL,
     axiom character varying COLLATE pg_catalog."default" NOT NULL,
+    name character varying COLLATE pg_catalog."default" NOT NULL,
     profile_id integer NOT NULL,
     CONSTRAINT "LSystem_pkey" PRIMARY KEY (lsystem_id),
+    CONSTRAINT "LSystem_name_profile_id_key" UNIQUE (name, profile_id),
     CONSTRAINT profile_id FOREIGN KEY (profile_id)
         REFERENCES public."Profile" (profile_id) MATCH SIMPLE
         ON UPDATE NO ACTION
